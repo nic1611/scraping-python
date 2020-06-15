64c286b891258187de2cbc139ecc520b27a5f387
Nicolas <nisoffi@gmail.com>
2020-06-15 16:06:55 -0300

    finalizando scraping do site

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-res = requests.get("https://www.abapzombie.com/")
-res.encoding = "utf-8"
-soup = BeautifulSoup(res.text, 'html.parser')
-items = soup.find_all(class_="bam-entry")
-
 all_items = []
-for item in items:
-    thumb = item.a.img["src"]
-    link = item.a.get('href'),
-    title = item.h2.text,
-    description = item.p
-    all_items.append({
-        "thumb": thumb,
-        "link": link,
-        "title": title,
-        "description": description.text
-    })
+url = "https://www.abapzombie.com/page/1/"
 
-print(items)
-print(all_items)
+for i in range(21):
+    res = requests.get(url)
+    res.encoding = "utf-8"
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    page = soup.find(class_="next").get('href')
+    url = page
+    items = soup.find_all(class_="bam-entry")
+
+    for item in items:
+        thumb = item.a.img["src"]
+        link = item.a.get('href'),
+        title = item.h2.text,
+        description = item.p
+        all_items.append({
+            "thumb": thumb,
+            "link": link,
+            "title": title,
+            "description": description.text
+        })
 
 with open('posts.json', 'w') as json_file:
-    json.dump(all_items, json_file, indent=3, ensure_ascii=False)
+    json.dump(all_items, json_file, indent=4)
